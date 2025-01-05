154ff566e251eb670b101cb8144171c0980aafd9
Xavier <xavier.roy-pomerleau.1@ulaval.ca>
2025-01-05 15:11:09 -0500

    Changed colors and layering in Scene 2

--- CentripetalAcceleration.py
+++ CentripetalAcceleration.py
@@ -2,6 +2,8 @@ from manim import *
 
 # manim -qh -p CentripetalAcceleration.py CentripetalAcceleration1
 
+config.background_color = WHITE
+
 class CentripetalAcceleration1(Scene):
     """
     This animation displays a particle going around a circle
@@ -32,7 +34,7 @@ class CentripetalAcceleration2(Scene):
     """
     This animation displays a particle following a trajectory shaped as a regular polygone since it's reflected
     each time it collides with the boundary. The animation shows how the orientation of the velocity vector is modified
-    at each collision by an pseudo-(instaneous acceleration) vector.
+    at each collision by a pseudo-(instaneous acceleration) vector.
 
     The number of sides of the polygone can be increased. At higher values, we start to see that the velocity vector
     is closer to the tangent of the circle.
@@ -40,22 +42,22 @@ class CentripetalAcceleration2(Scene):
     def construct(self):
         #plane = NumberPlane()
         #self.add(plane)
-        particle = Dot([2,0,0])
-        self.add(particle)
-
+        particle = Dot([2,0,0], color=RED)
+        particle.set_z_index(2)
         #Point of reference when we begin a segment of the trajectory (a side of the polygone)
         ref_begin = [2, 0, 0]
-        velocityVector = Arrow(start=ref_begin, end=[2, 1, 0], buff=0)
+        velocityVector = Arrow(start=ref_begin, end=[2, 1, 0], buff=0, color=BLACK)
 
         #Chooses the number of sides of the polygone
-        nbSides = 32
-        circTraj = Circle(radius=2)
+        nbSides = 8
+        circTraj = DashedVMobject(Circle(radius=2, color=BLACK), num_dashes=120)
+
 
         #Rotates the velocity vector so it matches the direction of the first segment.
         velocityVector.rotate(PI/nbSides, about_point=velocityVector.get_start())
         velocityVector.generate_target()
 
-        self.add(circTraj, velocityVector)
+        self.add(circTraj, velocityVector, particle)
         self.wait(0.5)
 
         for i in range(1, nbSides + 1):
@@ -63,15 +65,15 @@ class CentripetalAcceleration2(Scene):
             #End position of the current side of the polygone being completed
             ref_end = 2*np.array([np.cos(i*2*PI/nbSides), np.sin(i*2*PI/nbSides), 0])
 
-            currentSide = Line(ref_begin, ref_end)
+            currentSide = Line(ref_begin, ref_end, color=GRAY)
 
             #We only want to display the modification of the orientation of the velocity vector a few times
-            if i < nbSides/8:
+            if i < nbSides/2:
 
-                velocityVectorAtEnd = Arrow(start=ref_end, end=ref_end+np.array(velocityVector.get_vector()), buff=0)
-                self.play(AnimationGroup(particle.animate(rate_func=linear, run_time=0.2).move_to(ref_end),
-                                         Transform(velocityVector, velocityVectorAtEnd, rate_func=linear, run_time=0.2),
-                                         Create(currentSide, rate_func=linear, run_time=0.2)))
+                velocityVectorAtEnd = Arrow(start=ref_end, end=ref_end+np.array(velocityVector.get_vector()), buff=0, color=BLACK)
+                self.play(AnimationGroup(Transform(velocityVector, velocityVectorAtEnd, rate_func=linear, run_time=0.2),
+                                         Create(currentSide, rate_func=linear, run_time=0.2),
+                                        particle.animate(rate_func=linear, run_time=0.2).move_to(ref_end)))
 
                 #This object is used to determine the acceleration vector. "velocityVector" is now actually the same
                 #as velocityVectorAtEnd because of how the "Transform" animation works currently
@@ -83,12 +85,12 @@ class CentripetalAcceleration2(Scene):
                 vref = Vector(velocityAfterCollision.get_end() - velocityVector.get_end())
 
                 #Builds the acceleration vector that will be displayed on the particle
-                accVect1 = Arrow(start=velocityVector.get_start(), end=velocityVector.get_start() + vref.get_vector(), buff=0)
+                accVect1 = Arrow(start=velocityVector.get_start(), end=velocityVector.get_start() + vref.get_vector(), buff=0, color=BLUE)
 
                 #Builds the acceleration vector that will be displayed at the tip of the velocity vector
-                accVect2 = Arrow(start=velocityVector.get_end(), end=velocityAfterCollision.get_end(), buff=0)
+                accVect2 = Arrow(start=velocityVector.get_end(), end=velocityAfterCollision.get_end(), buff=0, color=BLUE)
 
-                line_to_center = DashedLine(velocityVector.get_start(), [0, 0, 0])
+                line_to_center = DashedLine(velocityVector.get_start(), [0, 0, 0], color=BLACK)
                 self.play(AnimationGroup(FadeIn(line_to_center), FadeIn(accVect1), run_time=0.5))
 
                 self.play(ReplacementTransform(accVect1, accVect2), run_time = 0.5)
@@ -99,7 +101,7 @@ class CentripetalAcceleration2(Scene):
             else:
                 #Rest of the animation where the particle moves at constant velocity around the trajectory
                 self.remove(velocityVector)
-                velocityVector.rotate(angle=2*PI/nbSides, about_point=velocityVector.get_start())
+                #velocityVector.rotate(angle=2*PI/nbSides, about_point=velocityVector.get_start())
                 self.play(AnimationGroup(particle.animate(rate_func=linear, run_time=0.2).move_to(ref_end),
                                          Create(currentSide, rate_func=linear, run_time=0.2)))
 
