ac95827f7412309d0cf06dc7b0a8027458260ca4
Xavier <xavier.roy-pomerleau.1@ulaval.ca>
2025-01-05 21:18:57 -0500

    Add sketch of angular acceleration animation

--- AngularVariables.py
+++ AngularVariables.py
@@ -1,5 +1,6 @@
 from Tools.scripts.texi2html import increment
 from manim import *
+from manim.utils.rate_functions import ease_in_sine, ease_in_cubic
 
 # manim -qh -p AngularVariables.py AngularVelocity
 
@@ -53,5 +54,38 @@ class AngularVelocity(Scene):
         outerLine = Line([-PI*3, 0, 0], [PI*3, 0, 0], color=RED)
 
         self.play(Transform(innerCirc, innerLine), Transform(outerCirc, outerLine))
+
+        self.wait()
+
+class AngularAcceleration(Scene):
+    """
+    This animation displays a particle going around a circle
+    at constant velocity with its velocity and acceleration vectors
+    """
+    def construct(self):
+        circTraj = Circle(radius=3, color=BLACK)
+        centerDot = Dot([0,0,0], color=BLACK)
+
+        self.add(circTraj, centerDot)
+
+        radius = Line(start=[0,0,0], end=[3,0,0], color=BLACK)
+        dotOuter = Dot([3, 0, 0], color=RED)
+        dotInner = Dot([1.5, 0, 0], color=BLUE)
+        dotOuter.set_z_index(1)
+        dotInner.set_z_index(1)
+
+        theta = 2*PI
+
+        innerArc = Arc(radius=1.5, angle=theta, color=GREEN)
+        outerArc = Arc(radius=3, angle=theta, color=GREEN)
+        xAxis = DashedLine([0,0,0], [3, 0, 0], color=BLACK)
+
+        self.add(xAxis, radius, dotOuter, dotInner)
+
+        self.play(AnimationGroup(Create(innerArc, rate_func=ease_in_cubic),
+                                 Create(outerArc, rate_func=ease_in_cubic),
+                                 Rotate(radius, angle=theta, about_point=ORIGIN, rate_func=ease_in_cubic),
+                                 Rotate(dotInner, angle=theta, about_point=ORIGIN, rate_func=ease_in_cubic),
+                                 Rotate(dotOuter, angle=theta, about_point=ORIGIN, rate_func=ease_in_cubic), run_time=6))
 
         self.wait()
